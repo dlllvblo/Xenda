a1dec66682c20ce525ad088062c017baa1da6a5a
Yoltic9601 <diazedgar1701@gmail.com>
2026-05-22 21:11:15 -0600

    abierto

--- app.py
+++ app.py
@@ -902,17 +902,17 @@ def index():
 
     exportar_excel_mensual()
 
-    if (
-        not registro_habilitado()
-        and
-        session.get('usuario') != ADMIN_CORREO
-):
-
-        session.clear()
-
-        return render_template(
-            'cerrado.html'
-    )
+#    if (
+#        not registro_habilitado()
+#        and
+#        session.get('usuario') != ADMIN_CORREO
+#):
+#
+#        session.clear()
+#
+#        return render_template(
+#            'cerrado.html'
+#    )
 
     if 'usuario' not in session:
 
